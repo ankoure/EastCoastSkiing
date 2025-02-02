5d3e3383c6c83eb5fd4f280a4fb56ed6b9a4c97e
ankoure <50218177+ankoure@users.noreply.github.com>
2025-02-01 19:28:40 -0500

    Create GenerateGeojson.sql

---
+++ python/GenerateGeojson.sql
@@ -0,0 +1,12 @@
+SELECT jsonb_build_object(
+    'type',     'FeatureCollection',
+    'features', jsonb_agg(feature)
+)
+FROM (
+  SELECT jsonb_build_object(
+    'type',       'Feature',
+    'id',         resort_pk,
+    'geometry',   ST_AsGeoJSON(geom)::jsonb,
+    'properties', to_jsonb(row) - 'gid' - 'geom'
+  ) AS feature
+  FROM (SELECT * FROM resort) row) features;
